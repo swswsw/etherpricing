8ee9912f1490dab0541195eea0c1ea44bf339cec
sol wu <apiswswsw@gmail.com>
2016-03-13 14:54:55 -0700

    new exchange: livecoin.  this works on cloud, but does not work on local dev server (v1.9.7).  temporarily not enable this by writing this to cache.  javax.net.ssl.SSLHandshakeException: Could not verify SSL certificate for URL: https://api.livecoin.net/exchange/ticker?currencyPair=ETH/BTC

---
+++ etherpricing/src/com/etherpricing/net/Urlfetch.java
@@ -0,0 +1,55 @@
+package com.etherpricing.net;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.json.JSONObject;
+
+import com.google.appengine.api.urlfetch.FetchOptions;
+import com.google.appengine.api.urlfetch.HTTPMethod;
+import com.google.appengine.api.urlfetch.HTTPRequest;
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
+
+/**
+ * retrieve data using appengine's low level urlfetch.  
+ * this allows us to set some options that are otherwise unavailable with httpurlconnection.  
+ * options such as doNotValidateCertificate
+ * 
+ * @author sol wu
+ *
+ */
+public class Urlfetch {
+	
+	private static final Logger log = Logger.getLogger(Urlfetch.class.getName());
+
+	public static JSONObject jsonData(String sUrl) 
+		throws IOException {
+		JSONObject json = null;
+		try {
+//			HTTPRequest httpRequest = new HTTPRequest(new URL(sUrl), HTTPMethod.GET, FetchOptions.Builder.validateCertificate());
+//			URLFetchService service = URLFetchServiceFactory.getURLFetchService();
+//			HTTPResponse httpResponse = service.fetch(httpRequest);
+//			String content = new String(httpResponse.getContent());
+			
+			URLFetchService fetcher = URLFetchServiceFactory.getURLFetchService();
+	        FetchOptions lFetchOptions = FetchOptions.Builder
+	        		.doNotValidateCertificate()
+	        		.setDeadline(20D)
+	        		.doNotFollowRedirects();
+	        HTTPRequest request = new HTTPRequest(new URL(sUrl), HTTPMethod.GET, lFetchOptions);
+	        HTTPResponse response = fetcher.fetch(request);
+	        String content = new String(response.getContent());
+			
+			json = new JSONObject(content);
+		} catch (MalformedURLException ex) {
+			log.log(Level.WARNING, "malformed url.  this should not happen as the input should have be vetted", ex);
+		}
+		
+		return json;
+	}
+}
---
+++ etherpricing/war/update/livecoin.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@include file="updateheader.jspf" %>
+<%@ page import="java.util.*" %>
+<%@ page import="com.etherpricing.net.*" %>
+<%@ page import="com.etherpricing.cache.*" %>
+<%@ page import="org.json.*" %>
+<%
+// note: this works on cloud, but does not work on local dev server (v1.9.7).  javax.net.ssl.SSLHandshakeException: Could not verify SSL certificate for URL: https://api.livecoin.net/exchange/ticker?currencyPair=ETH/BTC
+JSONObject json = RetrieveData.jsonData("https://api.livecoin.net/exchange/ticker?currencyPair=ETH/BTC");
+final long time = System.currentTimeMillis();
+final String livecoin = "Livecoin"; 
+PriceCache pc = new PriceCache();
+if (json != null) {
+	PriceCache.Price price = 
+		new PriceCache.Price("ETH", "BTC", json.getDouble("last"), json.getDouble("volume"), time, livecoin);
+	
+	pc.getPriceList().add(price);
+	
+	// todo: temp
+	//CacheManager.save("latest_livecoin", pc);
+}
+%>
+<%=pc%>
